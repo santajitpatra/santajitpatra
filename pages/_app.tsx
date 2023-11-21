88f0c1113e8852abc338c6479980d78d8b7caf3a
Santajit Patra <business.santajitpatra@gmail.com>
2023-11-21 23:38:35 +0530

    fix : top loader
    add: nextjs-toploader
    remove :@badrap/bar-of-progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,9 @@
 import "@/styles/globals.css";
 import "@/styles/animation.css";
 import type { AppProps } from "next/app";
-import ProgressBar from "@badrap/bar-of-progress";
-import { Router } from "next/router";
 import { SessionProvider } from "next-auth/react";
+import NextTopLoader from "nextjs-toploader";
 
-const progress = new ProgressBar({
-  size: 4,
-  color: "#22d3ee",
-  className: "z-50",
-  delay: 100,
-});
-
-Router.events.on("routeChangeStart", progress.start);
-Router.events.on("routeChangeComplete", progress.finish);
-Router.events.on("routeChangeError", progress.finish);
 
 export default function App({
   Component,
@@ -22,6 +11,12 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <NextTopLoader
+        color="#2299DD"
+        height={4}
+        showSpinner={false}
+
+      />
       <Component {...pageProps} />
     </SessionProvider>
   );
